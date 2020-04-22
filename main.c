c58c0b581f56f065c1228301af11f4e2e3a76af9
OldBoots <oleg.vezuviy@mail.com>
2020-04-22 16:14:20 +0600

    Test trevis

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+
 int main()
 {
-  printf("Hollow HUY!\n");
+	int i, n = 8;
+    for (i = 0; i < n; i++) {
+        if (i % 2 == 0)
+            printf("Fork");
+        if (i % 2 != 0)
+            printf(" - Fuck.\n");
+    }
     return 0;
 }
 
